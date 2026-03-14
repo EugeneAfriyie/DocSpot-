a9b9fa63acd3f7cb671b6164090440f76d3ccb6f
Eugene Afriyie <groupeight00@gmail.com>
2026-03-14 00:15:11 +0000

    Add admin login functionality with JWT authentication

--- Backend/Controllers/adminController.js
+++ Backend/Controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator';
 import bycrypt, { hash } from 'bcrypt';
 import {v2 as cloudinary} from 'cloudinary';
 import doctorModel from '../model/doctorModel.js';
+import jwt from 'jsonwebtoken';
 
 
 // APIFOR ADDING DOCTORS
@@ -78,5 +79,30 @@ const addDoctor = async (req, res ) =>{
     }
 }
 
+// api for admin login 
 
-export  {addDoctor};
+const adminLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign( email+password , process.env.JWT_SECRET); 
+
+            return res.status(200).json({ success: true, message: 'Admin logged in successfully', token });
+        }else {
+            return res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+
+        // if (!email || !password) {
+        //     return res.status(400).json({ success: false, message: 'Email and password are required' });
+        // }
+    } catch (error) {
+        console.error('Error logging in admin:', error);
+        res.status(500).json({ success: false, message: error.message || 'Server error' });
+    }
+
+
+}
+
+
+export  {addDoctor,adminLogin}; 
--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,6 +1,6 @@
 import express  from 'express';
 import upload from '../Middleware/multer.js';
-import { addDoctor } from '../Controllers/adminController.js';
+import { addDoctor, adminLogin } from '../Controllers/adminController.js';
 
 
 
@@ -8,6 +8,6 @@ import { addDoctor } from '../Controllers/adminController.js';
 const adminRouter = express.Router();
 
 adminRouter.post('/add-doctor', upload.single('image'), addDoctor)
-
+adminRouter.post('/admin-login', adminLogin)
 
 export {adminRouter}
